5fde49d4f9903b09d5db4a242c7b546c46223b20
Qiang Zhang <qzhang0130@gmail.com>
2020-04-12 23:02:22 -0400

    Finish the basic version of code Add to Visual Studio code find the compile command

--- 1_Trie_structure/test.cpp
+++ 1_Trie_structure/test.cpp
@@ -5,8 +5,8 @@ int main(){
 	Trie root;
 	std::string test = "abc";
 	root.insert(test);
-	/*cout<<root.find(string("bcd"))<<endl;
-	cout<<root.find(string("abc"))<<endl;
-	cout<<root.startWith(string("ab"))<<endl;*/
+	std::cout<<root.find("bcd")<<std::endl;
+	std::cout<<root.find("abc")<<std::endl;
+	std::cout<<root.startWith("ab")<<std::endl;
 	return 0;
 }
