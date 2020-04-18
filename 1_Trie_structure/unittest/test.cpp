46dfb1b665abb1ead1df73d7691a57e800edcce0
Qiang Zhang <qzhang0130@gmail.com>
2020-04-18 18:59:27 -0400

    Add catch2 test case; Add cmake file for test caces; Add gitignore to ignore cmake generated files;

--- 1_Trie_structure/unittest/test.cpp
+++ 1_Trie_structure/unittest/test.cpp
@@ -1,8 +1,12 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-
+#include <vector>
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
+}
+int getValue(){
+    std::vector<int> a(10, 1);
+    return a[10];
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
@@ -10,4 +14,5 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
     REQUIRE( Factorial(10) == 3628800 );
+    // REQUIRE_THROWS
 }
