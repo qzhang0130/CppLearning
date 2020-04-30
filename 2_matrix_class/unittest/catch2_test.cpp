1644d8878f47e1ce4b36af2b2db2182d7da56bb7
Qiang Zhang <qzhang0130@gmail.com>
2020-04-30 19:20:35 -0400

    Add more test cases

--- 2_matrix_class/unittest/catch2_test.cpp
+++ 2_matrix_class/unittest/catch2_test.cpp
@@ -13,7 +13,23 @@ TEST_CASE( "Basic Matrix Structure Test", "[general]"){
 	Matrix mat2(vec2);
 	std::pair<int, int> p1{2, 3};
 	REQUIRE(mat2.getDimensions() == p1);
+}
 
+TEST_CASE( "Matrix addition subtraction", "[general]"){
+	std::vector<std::vector<int>> vec1{{1,2}, {3,4}};
+	Matrix mat1(vec1);
 
-    REQUIRE(true == true);
+	std::vector<std::vector<int>> vec2{{1,2}, {3,4}};
+	Matrix mat2(vec2);
+
+	Matrix mat3 = mat1 + mat2;
+
+	auto res = mat3.getVector();
+	std::vector<std::vector<int>> ans{{2, 4}, {6, 8}};
+	REQUIRE(res == ans);
+
+	Matrix mat4 = mat1 - mat2;
+	ans = {{0, 0}, {0, 0}};
+	res = mat4.getVector();
+	REQUIRE(ans == res);
 }
