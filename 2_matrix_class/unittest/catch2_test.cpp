8095d86a93b579744eeb8fa6b062cd2656dd34dc
Qiang Zhang <qzhang0130@gmail.com>
2020-05-03 14:26:33 -0400

    Make corresponding changes to the catch2 according to the matrix template

--- 2_matrix_class/unittest/catch2_test.cpp
+++ 2_matrix_class/unittest/catch2_test.cpp
@@ -6,30 +6,30 @@
 
 TEST_CASE( "Basic Matrix Structure Test", "[general]"){
 	std::vector<std::vector<int>> vec1{{1,2}, {3,4}};
-	Matrix mat1(vec1);
+	Matrix<int> mat1(vec1);
 	std::pair<int, int> p{2, 2};
 	REQUIRE(mat1.getDimensions() == p);
 
 	std::vector<std::vector<int>> vec2{{1,2, 3}, {3,4, 5}};
-	Matrix mat2(vec2);
+	Matrix<int> mat2(vec2);
 	std::pair<int, int> p1{2, 3};
 	REQUIRE(mat2.getDimensions() == p1);
 }
 
 TEST_CASE( "Matrix addition subtraction", "[general]"){
 	std::vector<std::vector<int>> vec1{{1,2}, {3,4}};
-	Matrix mat1(vec1);
+	Matrix<int> mat1(vec1);
 
 	std::vector<std::vector<int>> vec2{{1,2}, {3,4}};
-	Matrix mat2(vec2);
+	Matrix<int> mat2(vec2);
 
-	Matrix mat3 = mat1 + mat2;
+	Matrix<int> mat3 = mat1 + mat2;
 
 	auto res = mat3.getVector();
 	std::vector<std::vector<int>> ans{{2, 4}, {6, 8}};
 	REQUIRE(res == ans);
 
-	Matrix mat4 = mat1 - mat2;
+	Matrix<int> mat4 = mat1 - mat2;
 	ans = {{0, 0}, {0, 0}};
 	res = mat4.getVector();
 	REQUIRE(ans == res);
@@ -37,12 +37,12 @@ TEST_CASE( "Matrix addition subtraction", "[general]"){
 
 TEST_CASE( "Matrix multiplication", "[general]"){
 	std::vector<std::vector<int>> vec1{{1,2}, {3,4}};
-	Matrix mat1(vec1);
+	Matrix<int> mat1(vec1);
 
 	std::vector<std::vector<int>> vec2{{1,2}, {3,4}};
-	Matrix mat2(vec2);
+	Matrix<int> mat2(vec2);
 
-	Matrix mat3 = mat1 * mat2;
+	Matrix<int> mat3 = mat1 * mat2;
 
 	auto res = mat3.getVector();
 	std::vector<std::vector<int>> ans{{7, 10}, {15, 22}};
@@ -50,10 +50,10 @@ TEST_CASE( "Matrix multiplication", "[general]"){
 
 	vec1 = {{1, 2, 3}, {4, 5, 6}};
 	vec2 = {{3}, {2}, {1}};
-	Matrix mat4(vec1);
-	Matrix mat5(vec2);
+	Matrix<int> mat4(vec1);
+	Matrix<int> mat5(vec2);
 
-	Matrix mat6 = mat4*mat5;
+	Matrix<int> mat6 = mat4*mat5;
 	res = mat6.getVector();
 	ans = {{10}, {28}};
 	REQUIRE(res == ans);
