115e8d6f8da6bf1a934c6091aefc5ee5c84d03f2
Qiang Zhang <qzhang0130@gmail.com>
2020-06-02 08:30:39 -0400

    Finish implementing log class

--- 3_design_patterns/General/Log.cpp
+++ 3_design_patterns/General/Log.cpp
@@ -1,22 +1,47 @@
 #include <iostream>
 #include <string>
+#include <memory>
 
-class ILogger{
-public:
+struct ILogger{
 	virtual void output(std::string s) = 0;
 };
 
-class ConsoleLogger: public ILogger{
-public:
+struct ConsoleLogger final: public ILogger{
 	void output(std::string s) override{
 		std::cout<<s<<std::endl;
 	}
 };
 
+class Engine{
+private:
+	int horsepower = 400;
+	float volume = 4;
+public:
+	friend std::ostream& operator<<(std::ostream& os, const Engine& e1){
+		os << "Horsepower: " << e1.horsepower <<" Volume: " << e1.volume;
+		return os;
+	}
+};
+
+class Car{
+private:
+	std::shared_ptr<ILogger> logger;  //= std::make_shared<ConsoleLogger>();
+	std::shared_ptr<Engine> engine; // = std::make_shared<Engine>();
+public:
+	Car(const std::shared_ptr<ILogger>& logger, const std::shared_ptr<Engine>& engine): logger(logger), engine(engine){}
+
+	friend std::ostream& operator<<(std::ostream& os, const Car& car){
+		os << "Car with Engine "<< *(car.engine) << "\n";
+		return os;
+	}
+};
+
 
 int main(){
-	ConsoleLogger cl;
-	std::string s {"abc"};
-	cl.output(s);
+	std::shared_ptr<ILogger> logger = std::make_shared<ConsoleLogger>();
+	std::shared_ptr<Engine> engine = std::make_shared<Engine>();
+
+	Car car(logger, engine);
+	std::cout<<car<<std::endl;
 	return 0;
 }
