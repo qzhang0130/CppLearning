1eb87abd0441135fca6ae3a942b9ee5d4d39ad9a
Qiang Zhang <qzhang0130@gmail.com>
2020-06-08 09:48:57 -0400

    Add bridge design pattern

---
+++ 3_design_patterns/Bridge/Person.cpp
@@ -0,0 +1,20 @@
+struct Person::PersonImpl{
+	void greet(Person* p);
+};
+
+void Person::PersonImpl::greet(Person* p){
+	printf("Hello, my name is %s", p->name.c_str());
+}
+
+Person::Person():
+impl(new PersonImpl){
+
+}
+
+Person::~Person{
+	delete impl;
+}
+
+void Person::greet(){
+	impl->greet(this);
+}
---
+++ 3_design_patterns/Bridge/Person.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+struct Person{
+	std::string name;
+
+	struct PersonImpl;
+	PersonImpl *impl;
+
+	Person();
+	~Person();
+
+	void greet();
+};
